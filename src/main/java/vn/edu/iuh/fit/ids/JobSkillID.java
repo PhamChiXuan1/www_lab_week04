5c5aee0ecdf93353e391139099f6497a9b3b7114
PhamChiXuan1 <99774231+PhamChiXuan1@users.noreply.github.com>
2023-11-20 18:31:33 +0700

    sửa models, enums

---
+++ src/main/java/vn/edu/iuh/fit/ids/JobSkillID.java
@@ -0,0 +1,29 @@
+package vn.edu.iuh.fit.ids;
+
+import vn.edu.iuh.fit.models.Job;
+import vn.edu.iuh.fit.models.Skill;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class JobSkillID implements Serializable {
+    private Skill skill;
+    private Job job;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JobSkillID that = (JobSkillID) o;
+        if(!skill.equals(that.skill)) return false;
+        return job.equals(that.job);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = skill.hashCode();
+        result = 31 * result + job.hashCode();
+        return result;
+    }
+}
